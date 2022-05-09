bd178ff034988a753d6670dcd997fa2b2b9b70fd
Anupam Anand <aanand20@iitk.ac.in>
2022-05-09 17:57:24 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from "react";
+import match from "./matches.json";
+import "./App.css";
+import "jquery/dist/jquery.min.js";
+import "datatables.net-dt/js/dataTables.dataTables";
+import "datatables.net-dt/css/jquery.dataTables.min.css";
+import $ from "jquery";
+import image from "./ipl.jpg"
+
+function App() {
+  const [data, setData] = useState(match);
+  useEffect(() => {
+    $(document).ready(function () {
+      $("#example tfoot tr th ").each(function () {
+        var title = $(this).text();
+        $(this).html(
+          '<input type="text" placeholder="Search ' + title + '" />'
+        );
+      });
+
+      // DataTable
+      var table = $("#example").DataTable({
+        initComplete: function () {
+          // Apply the search
+          this.api()
+            .columns()
+            .every(function () {
+              var that = this;
+
+              return $("input", this.footer()).on(
+                "keyup change clear",
+                function () {
+                  if (that.search() !== this.value) {
+                    that.search(this.value).draw();
+                  }
+                }
+              );
+            });
+        },
+        bDestroy: true,
+      });
+    });
+  });
+  return (
+    <>
+      <div class="topnav">
+      <a class="navbar-brand" href="/">
+      <div class="logo-image">
+      <img src = {image} />
+      </div>
+</a>
+        <a class="active" href="#home">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
+      <table
+        id="example"
+        className="table table-hover table-bordered cell-border display compact"
+        width="100%"
+      >
+        <thead>
+          <tr>
+            <th style={{width:"50%"}}>ID</th>
+            <th>season</th>
+            <th>date</th>
+            <th>City</th>
+            <th>Team 1</th>
+            <th>Team 2</th>
+            <th>Winner</th>
+            <th>win_by_runs</th>
+            <th>win_by_wickets</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, id) => {
+            return (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.season}</td>
+                <td>{item.date}</td>
+                <td>{item.city}</td>
+                <td>{item.team1}</td>
+                <td>{item.team2}</td>
+                <td>{item.winner}</td>
+                <td>{item.win_by_runs}</td>
+                <td>{item.win_by_wickets}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot>
+          <tr>
+            <th>ID</th>
+            <th>season</th>
+            <th>date</th>
+            <th>City</th>
+            <th>Team 1</th>
+            <th>Team 2</th>
+            <th>Winner</th>
+            <th>win_by_runs</th>
+            <th>win_by_wickets</th>
+          </tr>
+        </tfoot>
+      </table>
+    </>
+  );
+}
+export default App;
